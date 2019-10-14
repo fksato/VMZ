06e1f266df41444a46b211324035078851a891e3
fksato <f.k.sato@tum.de>
2019-10-14 12:47:41 -0700

    Update repo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name='vmz_interface',
 	version='0.1.0',
 	packages=find_packages(exclude=['tests']),
-	url='https://github.com/fksato/VMZ',
+	url='https://github.com/fksato/vmz_interface',
 	author='Fukushi Sato',
 	author_email='f.kazuo.sato@gmail.com',
 	description='Facebook VMZ interface',
@@ -21,11 +21,4 @@ setup(
 			        'Programming Language :: Python :: 3.6',
 			        'Programming Language :: Python :: 3.7',
 			    ],
-	# entry_points={
-	# 			'console_scripts':[
-	# 								'extract_features = tools.extract_features:main',
-	# 								'db_video_create = data.db_video_create:main'
-	# 					          ]
-
-	# 			}
 )
