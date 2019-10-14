bd4c31a9f1c825b76299049646daa2bc5bca62f9
fksato <f.k.sato@tum.de>
2019-10-14 14:05:13 -0700

    Change db video create script

--- vmz_interface/extractor/build_lmdb.py
+++ vmz_interface/extractor/build_lmdb.py
@@ -61,7 +61,7 @@ class VideoDBBuilder:
 			for i in range(len(lmdb_metas)):
 				with open(f'{self.clips_lmdb_data_path}/lmdb_meta_{i}.csv') as f:
 					df = pd.read_csv(f)
-				created_metas.add(set(df['org_video'].values.flatten()))
+				created_metas.update(set(df['org_video'].unique()))
 			if created_metas == vid_list:
 				self.video_lmdb_paths = glob(f'{self.clips_lmdb_data_path}/lmdb_*_db')
 			else:
